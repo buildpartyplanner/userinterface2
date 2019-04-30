a2572b5674581d67027e0ee26960f3b81d2e53ad
jzanderson09 <jzanderson09@gmail.com>
2019-04-29 21:47:36 -0600

    Added pop-up gallery div, will be adding stock images to cycle through for an image carousel. Added classes for gallery div and its buttons to cycle through.  Moving forward, will be adding specific styling for both tablet and mobile layouts via media queries. Added team and About Us link for additional pages, will add those pages moving forward.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,43 @@
-const galleryLink = document.querySelector('.gallery');
-const gallery = document.createElement('div');
-gallery.style.width = '70%';
-gallery.style.height = '500px';
-gallery.style.opacity = '0.6';
-gallery.style.backgroundColor = 'black';
-gallery.style.borderRadius = '5%';
-gallery.style.position = 'fixed';
-gallery.style.margin = '5% auto 5% 15%';
+//body selection:
 const bodyDiv = document.querySelector('body');
+
+//galleryDiv:
+const gallery = document.createElement('div');
+gallery.classList.add('galleryDiv');
+
+//gallery left button:
+const galleryLeft = document.createElement('button');
+galleryLeft.classList.add('galleryButtons');
+galleryLeft.textContent = '<';
+gallery.appendChild(galleryLeft);
+
+//gallery right button:
+const galleryRight = document.createElement('button');
+galleryRight.classList.add('galleryButtons');
+galleryRight.textContent = '>';
+gallery.appendChild(galleryRight);
+
+//container selection:
+const containerDiv = document.querySelector('.container');
+//gallery link selection (nav):
+const galleryLink = document.querySelector('.gallery');
+//gallery buttons selection:
+const galleryButtons = document.querySelectorAll('.galleryButtons');
+
+//click to gallery link in nav, prepends pop-up gallery div:
 galleryLink.addEventListener('click', (event) => {
-    event.stopPropagation(bodyDiv);
+    event.stopPropagation(containerDiv);
     bodyDiv.prepend(gallery);
 });
-bodyDiv.addEventListener('click', () => {
+
+//general click to the body, removes pop-up gallery div:
+containerDiv.addEventListener('click', () => {
     bodyDiv.removeChild(gallery);
 });
+
+//click to gallery buttons, preventing propagation to the body:
+galleryButtons.forEach(addEventListener('click', (event) => {
+    event.stopPropagation(containerDiv);  
+}));
+
+//image carousel:
